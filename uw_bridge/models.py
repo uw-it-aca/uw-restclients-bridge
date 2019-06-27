8aea6a8b2bb00d9e0cab1b8ca1def6b023c29529
Fang Lin <fanglin@uw.edu>
2019-06-27 12:06:10 -0700

    clean up

--- uw_bridge/models.py
+++ uw_bridge/models.py
@@ -16,8 +16,6 @@ class BridgeCustomField(models.Model):
     POS1_ORG_CODE = "pos1_org_code"
     POS1_ORG_NAME = "pos1_org_name"
     POS1_UNIT_CODE = "pos1_unit_code"
-    POS1 = [POS1_BUDGET_CODE, POS1_JOB_CODE, POS1_JOB_CLAS, POS1_LOCATION,
-            POS1_ORG_CODE, POS1_ORG_NAME, POS1_UNIT_CODE]
 
     POS2_BUDGET_CODE = "pos2_budget_code"
     POS2_JOB_CODE = "pos2_job_code"
@@ -26,8 +24,6 @@ class BridgeCustomField(models.Model):
     POS2_ORG_CODE = "pos2_org_code"
     POS2_ORG_NAME = "pos2_org_name"
     POS2_UNIT_CODE = "pos2_unit_code"
-    POS2 = [POS2_BUDGET_CODE, POS2_JOB_CODE, POS2_JOB_CLAS, POS2_LOCATION,
-            POS2_ORG_CODE, POS2_ORG_NAME, POS2_UNIT_CODE]
 
     field_id = models.CharField(max_length=10)
     name = models.CharField(max_length=64)
